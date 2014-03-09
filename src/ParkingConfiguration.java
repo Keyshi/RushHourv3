07e5042bbd0555b791cafc6ddb8debc8475d66d2
Xavier Leblond <xavier.leblond@ymail.com>
2014-03-09 15:39:36 +0000

    Add getAllMoves().

--- src/ParkingConfiguration.java
+++ src/ParkingConfiguration.java
@@ -8,7 +8,34 @@ public class ParkingConfiguration {
       this.p = p;
    }
 
+   /**
+    * Gives all the possible moves of a vehicule.
+    * @param Vehicule v the vehicule to check.
+    * @return the list of possible move.
+    */
+   public ArrayList<String> getAllMoves(Vehicule v)
+   {
+      ArrayList<String> possibleMove;
+      String direction = v.getDir();
+      String name = v.getNom();
+      int distance = 0;
+      String[] direction = {"L","R","U","D"};
+      for (int i = 0 ; i < direction.length ; i++) {
+         do {
+            distance = 1;
+            // TODO : Instancier ParkingController
+            isMovePossible = verifDeplacement(v, direction[i], distance);
+            if (isMovePossible == true) {
+               possibleMove.add(name + direction[i] + distance);
+            }
+            distance++;
+         } while (isMovePossible == true);
+      }
+      return possibleMove;
+   }
 
-   public void getParking(){};
+
+   public void getParking()
+   {};
 
 }
