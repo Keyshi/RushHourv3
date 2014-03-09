e5ebe4a3240d3706ebaadbda741aa79fb5dbe4f0
Xavier Leblond <xavier.leblond@ymail.com>
2014-03-09 00:11:03 +0000

    Add ParkingConfiguration class

---
+++ src/ParkingConfiguration.java
@@ -0,0 +1,14 @@
+public class ParkingConfiguration {
+
+   private int levelNumber;
+   private Parking p;
+
+   public ParkingConfiguration (Parking p) {
+      this.levelNumber = 0;
+      this.p = p;
+   }
+
+
+   public void getParking(){};
+
+}
